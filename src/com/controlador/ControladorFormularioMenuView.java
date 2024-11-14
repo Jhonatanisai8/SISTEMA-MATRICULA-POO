be07d2b0b451f9e45b9a7e55cd446065299e8e2a
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-13 23:09:09 -0500

    clase ControladorFormularioMenuView: agregacion del implements ActionListener

--- src/com/controlador/ControladorFormularioMenuView.java
+++ src/com/controlador/ControladorFormularioMenuView.java
@@ -1,15 +1,28 @@
 package com.controlador;
 
 import com.procesos.ProcesosFrmMenuView;
+import com.vista.frmInicioView;
 import com.vista.frmMenuView;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ControladorFormularioMenuView {
-    
-    private frmMenuView menuView;
-    
+public class ControladorFormularioMenuView implements ActionListener {
+
+    private frmMenuView frmenuView;
+
     public ControladorFormularioMenuView(frmMenuView menuView) {
-        this.menuView = menuView;
-        ProcesosFrmMenuView.presentarFormularioMenu(menuView);
+        this.frmenuView = menuView;
+        this.frmenuView.mnInicio.addActionListener(this);
+        ProcesosFrmMenuView.presentarFormularioMenu(this.frmenuView);
     }
-    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == this.frmenuView.mnInicio) {
+            frmInicioView frInicioView = new frmInicioView();
+            System.out.println("Holaaa");
+            // pasamos el frmMenuView existente al ControladorFormularioInicio
+            ControladorFormularioInicio cfi = new ControladorFormularioInicio(frInicioView, frmenuView);
+        }
+    }
 }
