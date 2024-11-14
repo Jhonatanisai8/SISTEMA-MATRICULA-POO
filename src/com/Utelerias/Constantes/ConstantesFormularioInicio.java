0d18bd6a3f4d76d2fc30c84096aa24dee0c1b71b
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-13 23:30:33 -0500

    interface ConstantesFormularioInicio; agregacion de constantes para el formulario frmInicioView y ademas del logo

--- src/com/Utelerias/Constantes/ConstantesFormularioInicio.java
+++ src/com/Utelerias/Constantes/ConstantesFormularioInicio.java
@@ -1,6 +1,17 @@
 package com.Utelerias.Constantes;
 
+import java.awt.Font;
+
 public interface ConstantesFormularioInicio {
 
-    final String nombreAdmin = "Jhon Doe";
+    final String TEXTO_PRESENTACION = "Bienvenido al Sistema de Matrícula Escolar."
+            + " \n Aquí puedes gestionar fácilmente estudiantes, "
+            + "\n docentes,  cursos y horarios para llevar un "
+            + "\n registro organizado y eficiente de "
+            + "\n tu institución.";
+    final String NOMBRE_ADMIN = "Jhon Doe";
+    final String RUTA_IMAGEN_LOGO_IE = "C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\IEjesusNazaret.jpg";
+    final Font FUENTE_TEXT_PRESENTACION = new Font("Montserrat", Font.BOLD, 25);
+    final Font FUENTE_TEXT_FECHA = new Font("Montserrat", Font.BOLD, 20);
+    final Font FUENTE_TEXT_ADMIN = new Font("Montserrat", Font.CENTER_BASELINE, 20);
 }
