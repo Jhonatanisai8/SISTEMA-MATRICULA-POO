768c4600bd3f08995e8a4154d55c5a8dbd8d188d
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-13 21:17:57 -0500

    clase ProcesosFrmMenuView: implementando la interface ConstantesFrmMenu

--- src/com/procesos/ProcesosFrmMenuView.java
+++ src/com/procesos/ProcesosFrmMenuView.java
@@ -1,10 +1,13 @@
 package com.procesos;
 
+import com.constantes.ConstantesFrmMenu;
 import com.vista.frmMenuView;
+import java.awt.Color;
 import javax.swing.ImageIcon;
 
-public class ProcesosFrmMenuView {
-    
+public class ProcesosFrmMenuView
+        implements ConstantesFrmMenu {
+
     public static void ponerIconosMenus(frmMenuView frenuView) {
         frenuView.mnInicio.setIcon(new ImageIcon("C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\Inicio.png"));
         frenuView.mnEstudiantes.setIcon(new ImageIcon("C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\Estudiante.png"));
@@ -16,9 +19,12 @@ public class ProcesosFrmMenuView {
         frenuView.mnHorarios.setIcon(new ImageIcon("C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\Horario.png"));
         frenuView.mnCursos.setIcon(new ImageIcon("C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\Curso.png"));
     }
-    
+
     public static void presentarFormularioMenu(frmMenuView frenuView) {
         ponerIconosMenus(frenuView);
+        frenuView.lblAdministrador.setText(nombreAdministrador);
+        frenuView.lblAdministrador.setForeground(Color.black);
         frenuView.setResizable(false);
+
     }
 }
