c30128aa0de8a065bd70317833fbfade5a1cb3c2
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-13 23:07:13 -0500

    clase ControladorFormularioInicio: controlador del formulario frmInicioView

---
+++ src/com/controlador/ControladorFormularioInicio.java
@@ -0,0 +1,17 @@
+package com.controlador;
+
+import com.procesos.ProcesosFormularioInicio;
+import com.vista.frmInicioView;
+import com.vista.frmMenuView;
+
+public class ControladorFormularioInicio {
+
+    private final frmInicioView frInicioView;
+    private final frmMenuView frMenuView;  
+
+    public ControladorFormularioInicio(frmInicioView frInicioView, frmMenuView frMenuView) {
+        this.frInicioView = frInicioView;
+        this.frMenuView = frMenuView;  
+        ProcesosFormularioInicio.presentarFormulario(frMenuView.dsktEscritorio, this.frInicioView);
+    }
+}
