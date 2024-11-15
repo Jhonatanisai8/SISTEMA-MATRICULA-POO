bb8a768f33c7378fc42cbcee472c90c118799a88
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-14 23:06:50 -0500

    remonbrando algunos metodos y ademas poniendo los iconos a los submenus de los items del formualrio frmMenuView

--- src/com/procesos/ProcesosFormularioMenuView.java
+++ src/com/procesos/ProcesosFormularioMenuView.java
@@ -8,7 +8,7 @@ import com.vista.frmMenuView;
 public class ProcesosFormularioMenuView
         implements ConstantesFormularioMenu {
 
-    public static void ponerIconosMenus(frmMenuView frenuView) {
+    public static void ponerIconosFormulario(frmMenuView frenuView) {
         frenuView.mnInicio.setIcon(ICONO_MN_INICIO);
         frenuView.mnEstudiantes.setIcon(ICONO_MN_ESTUDIANTES);
         frenuView.mnDocentes.setIcon(ICONO_MN_DOCENTES);
@@ -18,10 +18,24 @@ public class ProcesosFormularioMenuView
         frenuView.mnSalir.setIcon(ICONO_MN_SALIR);
         frenuView.mnHorarios.setIcon(ICONO_MN_HORARIOS);
         frenuView.mnCursos.setIcon(ICONO_MN_CURSOS);
+        //para los submenus
+        frenuView.jmnRegistrarEstudiante.setIcon(ICONO_JMN_REGISTRAR_ESTUDIANTE);
+        frenuView.jmnAdministrarEstudiantes.setIcon(ICONO_JMN_ADMINISTRAR_ESTUDIANTE);
+        frenuView.jmnRegistrarDocente.setIcon(ICONO_JMN_REGISTRAR_DOCENTE);
+        frenuView.jmnAdministrarDocentes.setIcon(ICONO_JMN_ADMINISTRAR_DOCENTES);
+        frenuView.jmnRegistrarSalon.setIcon(ICONO_JMN_REGISTRAR_SALON);
+        frenuView.jmnAdministrarSalones.setIcon(ICONO_JMN_ADMINISTRAR_SALONES);
+        frenuView.jmnRegistrarCurso.setIcon(ICONO_JMN_REGISTRAR_CURSO);
+        frenuView.jmnAdministrarCursos.setIcon(ICONO_JMN_ADMINISTRAR_CURSOS);
+        frenuView.jmnAsignarHorario.setIcon(ICONO_JMN_REGISTRAR_HORARIO);
+        frenuView.jmnAdministrarHorarios.setIcon(ICONO_JMN_ADMINISTRAR_HORARIOS);
+        frenuView.jmnEstudiantesMatriculados.setIcon(ICONO_JMN_LISTAR_ESTUDIANTES_MATRICULADOS);
+        frenuView.jmnDocentesAsignados.setIcon(ICONO_JMN_LISTAR_DOCENTES_ASIGNADOS);
+
     }
 
-    public static void presentarFormularioMenu(frmMenuView frenuView) {
-        ponerIconosMenus(frenuView);
+    public static void presentarFormulario(frmMenuView frenuView) {
+        ponerIconosFormulario(frenuView);
         frenuView.setResizable(false);
         frmInicioView frInicioView = new frmInicioView();
         ControladorFormularioInicio cfi = new ControladorFormularioInicio(frInicioView, frenuView);
