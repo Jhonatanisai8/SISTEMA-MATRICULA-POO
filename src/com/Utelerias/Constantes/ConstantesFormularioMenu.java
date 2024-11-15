5ce411f12d91c0fe9cb4c7bf061c592d8396088c
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-14 21:39:59 -0500

    Agregacion de la interface ConstantesFormularioMenu: que contendra las constantes a implementar el formulario

---
+++ src/com/Utelerias/Constantes/ConstantesFormularioMenu.java
@@ -0,0 +1,16 @@
+package com.Utelerias.Constantes;
+
+import javax.swing.ImageIcon;
+
+public interface ConstantesFormularioMenu {
+
+    final ImageIcon ICONO_MN_INICIO = new ImageIcon("C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\Inicio.png");
+    final ImageIcon ICONO_MN_ESTUDIANTES = new ImageIcon("C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\Estudiante.png");
+    final ImageIcon ICONO_MN_DOCENTES = new ImageIcon("C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\Profesor.png");
+    final ImageIcon ICONO_MN_SALONES = new ImageIcon("C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\Aula.png");
+    final ImageIcon ICONO_MN_MATRICULA = new ImageIcon("C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\Matricula.png");
+    final ImageIcon ICONO_MN_LISTADOS = new ImageIcon("C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\Listados.png");
+    final ImageIcon ICONO_MN_SALIR = new ImageIcon("C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\CerrarSesion.png");
+    final ImageIcon ICONO_MN_HORARIOS = new ImageIcon("C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\Horario.png");
+    final ImageIcon ICONO_MN_CURSOS = new ImageIcon("C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\Curso.png");
+}
