c3454984bc96d059b8dbb148c4569e925a536693
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-23 12:13:45 -0500

    clase ConstantesFormularioListadoEstudiantes: por el momento solo tiene variables de tipo icon

---
+++ src/com/Utelerias/Constantes/ConstantesFormularioListadoEstudiantes.java
@@ -0,0 +1,13 @@
+package com.Utelerias.Constantes;
+
+import javax.swing.ImageIcon;
+
+public interface ConstantesFormularioListadoEstudiantes {
+
+//iconos
+    final ImageIcon ICONO_BUSCAR = new ImageIcon("C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\BuscarEstudiante.png");
+    final ImageIcon ICONO_MODIFICAR = new ImageIcon("C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\EditarEstudiante.png");
+    final ImageIcon ICONO_ELIMINAR = new ImageIcon("C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\EliminarEstudiante.png");
+    
+
+}
