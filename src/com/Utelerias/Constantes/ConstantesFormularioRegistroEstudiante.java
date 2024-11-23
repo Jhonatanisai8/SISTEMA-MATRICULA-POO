4e91795a155614cd56203b93ee3451ae87f442c6
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-23 10:32:18 -0500

    ConstantesFormularioRegistroEstudiante: constantes para la validacion de los campos

--- src/com/Utelerias/Constantes/ConstantesFormularioRegistroEstudiante.java
+++ src/com/Utelerias/Constantes/ConstantesFormularioRegistroEstudiante.java
@@ -5,7 +5,7 @@ import javax.swing.ImageIcon;
 public interface ConstantesFormularioRegistroEstudiante {
 
     final ImageIcon ICONO_BOTON_GUARDAR = new ImageIcon("C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\RegistroEstudiantes.png");
-    final String[] PROVINCIAS = {"Amazonas", "Ancash", "Arequipa", "Ayacucho", "Cajamarca", "Callao", "Cuzco"};
-    final String[] TIPO_DOCUMENTO = {"Dni", "Carnet De Extranjeria"};
+    final String[] PROVINCIAS = {"=Seleccionar=", "Amazonas", "Ancash", "Arequipa", "Ayacucho", "Cajamarca", "Callao", "Cuzco"};
+    final String[] TIPO_DOCUMENTO = {"=Seleccionar=", "Dni", "Carnet De Extranjeria"};
 
 }
