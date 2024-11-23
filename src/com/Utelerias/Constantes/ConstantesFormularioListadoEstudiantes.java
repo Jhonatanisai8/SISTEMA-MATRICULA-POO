d244d78e667b047c2a33913eb0f71dd5c5ac3d74
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-23 12:38:28 -0500

    clase ConstantesFormularioListadoEstudiantes: agregacion de titulos para las columnas

--- src/com/Utelerias/Constantes/ConstantesFormularioListadoEstudiantes.java
+++ src/com/Utelerias/Constantes/ConstantesFormularioListadoEstudiantes.java
@@ -8,6 +8,8 @@ public interface ConstantesFormularioListadoEstudiantes {
     final ImageIcon ICONO_BUSCAR = new ImageIcon("C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\BuscarEstudiante.png");
     final ImageIcon ICONO_MODIFICAR = new ImageIcon("C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\EditarEstudiante.png");
     final ImageIcon ICONO_ELIMINAR = new ImageIcon("C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\EliminarEstudiante.png");
-    
+
+    //cabezeras de la tabla alumnos
+    final String TITULOS_COLUMNAS[] = {"Nº", "Nombre", "Apellido Paterno", "Apellido Materno", "Fecha Nacimiento", "Tipo Documento", "Nº de Dni", "Direccion", "Distrito", "Calle", "Nº Calle", "Provincia", "Email Personal", "Email Educativo", "Codigo Estudiante"};
 
 }
--- src/com/procesos/ProcesosFormularioListadoEstudiantes.java
+++ src/com/procesos/ProcesosFormularioListadoEstudiantes.java
@@ -1,24 +1,28 @@
 package com.procesos;
 
 import com.Utelerias.Constantes.ConstantesFormularioListadoEstudiantes;
+import com.procesos.Servicios.ServiciosEstudiante;
 import com.vista.frmListadoEstudiantesView;
 import javax.swing.JDesktopPane;
 
 public class ProcesosFormularioListadoEstudiantes
         implements ConstantesFormularioListadoEstudiantes {
-
+    
     private static void ponerIconosFormulario(frmListadoEstudiantesView frmEstudiantesView) {
         frmEstudiantesView.btnBuscar.setIcon(ICONO_BUSCAR);
         frmEstudiantesView.btnModificar.setIcon(ICONO_MODIFICAR);
         frmEstudiantesView.btnEliminar.setIcon(ICONO_ELIMINAR);
-
+        
     }
-
+    
     public static void presentarFormulario(JDesktopPane desktopPane, frmListadoEstudiantesView frmEstudiantesView) {
         ponerIconosFormulario(frmEstudiantesView);
+        ServiciosEstudiante.mostrarEstudiantesEnTabla(frmEstudiantesView, TITULOS_COLUMNAS);
         frmEstudiantesView.setTitle("Listado de Estudiantes");
         desktopPane.add(frmEstudiantesView);
         frmEstudiantesView.toFront();
+        
         frmEstudiantesView.setVisible(true);
+        
     }
 }
