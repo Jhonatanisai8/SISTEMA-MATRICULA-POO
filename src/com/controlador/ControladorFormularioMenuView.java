9f88c5b12dd3d2b98c0fa0985aca8575fb1aae42
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-22 23:59:54 -0500

    evento del menuItem que llama al formulario de registro de estudiantes

--- src/com/controlador/ControladorFormularioMenuView.java
+++ src/com/controlador/ControladorFormularioMenuView.java
@@ -3,6 +3,7 @@ package com.controlador;
 import com.procesos.ProcesosFormularioMenuView;
 import com.vista.frmInicioView;
 import com.vista.frmMenuView;
+import com.vista.frmRegistrarEstudiante;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -25,6 +26,11 @@ public class ControladorFormularioMenuView
             System.out.println("Holaaa");
             // pasamos el frmMenuView existente al ControladorFormularioInicio
             ControladorFormularioInicio cfi = new ControladorFormularioInicio(frInicioView, frmenuView);
+        }
+        if (e.getSource() == this.frmenuView.jmnRegistrarEstudiante) {
+            System.out.println("Hola Mundo Jmenu");
+            frmRegistrarEstudiante frEstudiante = new frmRegistrarEstudiante();
+            ControladorFormularioRegistroEstudiantes cfre = new ControladorFormularioRegistroEstudiantes(this.frmenuView, frEstudiante);
         }
     }
 }
