aaf166c303fdd707ffab49f71b17eb500a765549
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-24 01:34:01 -0500

    clase ProcesosFormularioListadoEstudiantes: nuevo metodo que nos permite obtener la lista de estudiantes

--- src/com/procesos/ProcesosFormularioListadoEstudiantes.java
+++ src/com/procesos/ProcesosFormularioListadoEstudiantes.java
@@ -1,8 +1,12 @@
 package com.procesos;
 
+import com.Modelo.entidades.DaoImplementacion.EstudianteReposioImple;
+import com.Modelo.entidades.Estudiante;
+import com.Modelo.entidades.InterfaceDao.Repositorio;
 import com.Utelerias.Constantes.ConstantesFormularioListadoEstudiantes;
 import com.procesos.Servicios.ServiciosEstudiante;
 import com.vista.frmListadoEstudiantesView;
+import java.util.List;
 import javax.swing.JDesktopPane;
 
 public class ProcesosFormularioListadoEstudiantes
@@ -12,12 +16,16 @@ public class ProcesosFormularioListadoEstudiantes
         frmEstudiantesView.btnBuscar.setIcon(ICONO_BUSCAR);
         frmEstudiantesView.btnModificar.setIcon(ICONO_MODIFICAR);
         frmEstudiantesView.btnEliminar.setIcon(ICONO_ELIMINAR);
+    }
 
+    public static List<Estudiante> listaEstudiantes() {
+        Repositorio<Estudiante> repo = new EstudianteReposioImple();
+        return repo.listar();
     }
 
     public static void presentarFormulario(JDesktopPane desktopPane, frmListadoEstudiantesView frmEstudiantesView) {
         ponerIconosFormulario(frmEstudiantesView);
-        ServiciosEstudiante.mostrarEstudiantesEnTabla(frmEstudiantesView, TITULOS_COLUMNAS);
+        ServiciosEstudiante.mostrarEstudiantesEnTabla(frmEstudiantesView, TITULOS_COLUMNAS, listaEstudiantes());
         frmEstudiantesView.setTitle("Listado de Estudiantes");
         desktopPane.removeAll();
         desktopPane.add(frmEstudiantesView);
