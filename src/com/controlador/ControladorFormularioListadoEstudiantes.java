c3ba22c2f8e9b1f31014e3bb00580c7da691c08e
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-24 02:46:27 -0500

    clase ControladorFormularioListadoEstudiantes: implementacion del evento del boton que nos permite buscar por dni

--- src/com/controlador/ControladorFormularioListadoEstudiantes.java
+++ src/com/controlador/ControladorFormularioListadoEstudiantes.java
@@ -1,10 +1,16 @@
 package com.controlador;
 
+import static com.Utelerias.Constantes.ConstantesFormularioListadoEstudiantes.TITULOS_COLUMNAS;
 import com.procesos.ProcesosFormularioListadoEstudiantes;
+import static com.procesos.ProcesosFormularioListadoEstudiantes.listaEstudiantes;
+import com.procesos.Servicios.ServiciosEstudiante;
 import com.vista.frmListadoEstudiantesView;
 import com.vista.frmMenuView;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ControladorFormularioListadoEstudiantes {
+public class ControladorFormularioListadoEstudiantes
+        implements ActionListener {
 
     private final frmListadoEstudiantesView frmEstudiantesView;
     private final frmMenuView frmMenuView1;
@@ -12,7 +18,21 @@ public class ControladorFormularioListadoEstudiantes {
     public ControladorFormularioListadoEstudiantes(frmListadoEstudiantesView frmEstudiantesView, frmMenuView frmMenuView1) {
         this.frmEstudiantesView = frmEstudiantesView;
         this.frmMenuView1 = frmMenuView1;
+        this.frmEstudiantesView.btnBuscar.addActionListener(this);
         ProcesosFormularioListadoEstudiantes.presentarFormulario(this.frmMenuView1.dsktEscritorio, this.frmEstudiantesView);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == this.frmEstudiantesView.btnBuscar) {
+            if (frmEstudiantesView.txtBuscar.getText().isBlank()) {
+                ServiciosEstudiante.mostrarEstudiantesEnTabla(frmEstudiantesView, TITULOS_COLUMNAS, listaEstudiantes());
+            } else {
+                ProcesosFormularioListadoEstudiantes.enviarEstudiante(this.frmEstudiantesView.btlData, Long.valueOf(frmEstudiantesView.txtBuscar.getText()));
+            }
+            System.out.println("Has dado click en el boton buscar....");
+        }
+
     }
 
 }
