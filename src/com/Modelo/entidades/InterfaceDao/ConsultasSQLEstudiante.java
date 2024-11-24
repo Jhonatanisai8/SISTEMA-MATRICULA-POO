9e68ffae14a60064810ebb7b3b8b8a7d4eb9bbe9
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-24 00:50:09 -0500

    clase ConsultasSQLEstudiante: agregando nueva consulta para el listado de estudiantes

--- src/com/Modelo/entidades/InterfaceDao/ConsultasSQLEstudiante.java
+++ src/com/Modelo/entidades/InterfaceDao/ConsultasSQLEstudiante.java
@@ -11,4 +11,25 @@ public interface ConsultasSQLEstudiante {
 
     final String SQL_INSERT_ESTUDIANTE = "INSERT INTO estudiante (id_estudiante,codigo_estudiante,email_educativo)"
             + " VALUES ((SELECT id_persona FROM persona ORDER BY id_persona DESC limit 1),?,?)";
+
+    final String SQL_SELECT_ESTUDIANTES = "SELECT "
+            + "	p.id_persona AS 'ID', "
+            + "    p.nombre AS 'Nombre', "
+            + "    p.apellido_paterno AS 'Apellido Paterno', "
+            + "    p.apellido_materno AS 'Apellido Materno', "
+            + "    p.fecha_nacimiento AS 'Fecha Nacimiento', "
+            + "    d.tipo_documento AS 'Tipo Documento', "
+            + "    d.numero_dni AS 'Nº de Dni', "
+            + "    di.calle AS 'Calle', "
+            + "    di.numero_cale AS 'Nº Calle', "
+            + "    di.distrito AS 'Distrito', "
+            + "    di.provincia AS 'Provincia', "
+            + "    p.telefono AS 'Telefono', "
+            + "    p.email_personal AS 'Email Personal', "
+            + "    e.codigo_estudiante AS 'Cod Estudiante', "
+            + "    e.email_educativo AS 'Email Educativo' "
+            + "FROM persona AS p "
+            + "INNER JOIN dni AS d ON d.id_dni = p.id_dni "
+            + "INNER JOIN direccion AS di ON di.id_direccion = p.id_direccion "
+            + "INNER JOIN estudiante AS e ON e.id_estudiante = p.id_persona";
 }
