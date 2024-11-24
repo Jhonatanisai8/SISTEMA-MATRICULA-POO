da4840fada59056d0d130bd5c78e838fedafab47
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-24 01:33:15 -0500

    clase ServiciosEstudiante: modificacion del metodo mostrarEstudiantesEnTabla para que nos cargue la informacion de los estudiantes

--- src/com/procesos/Servicios/ServiciosEstudiante.java
+++ src/com/procesos/Servicios/ServiciosEstudiante.java
@@ -1,6 +1,8 @@
 package com.procesos.Servicios;
 
+import com.Modelo.entidades.Estudiante;
 import com.vista.frmListadoEstudiantesView;
+import java.util.List;
 import java.util.Random;
 import javax.swing.table.DefaultTableModel;
 
@@ -16,8 +18,28 @@ public class ServiciosEstudiante {
         return String.valueOf(n).concat(nombre.substring(0, 3)).concat(apellido.substring(0, 3)).concat("EST2402").toUpperCase();
     }
 
-    public static void mostrarEstudiantesEnTabla(frmListadoEstudiantesView frmEstudiantesView, String titulosColumnas[]) {
-        DefaultTableModel modeloTabla = new DefaultTableModel(null, titulosColumnas);
+    public static void mostrarEstudiantesEnTabla(frmListadoEstudiantesView frmEstudiantesView, String titulosColumnas[], List<Estudiante> listaEstudiantes) {
+        Object data[][] = new Object[titulosColumnas.length][titulosColumnas.length];
+        for (int i = 0; i < frmEstudiantesView.btlData.getColumnCount(); i++) {
+            Estudiante estudiante = listaEstudiantes.get(i);
+            data[i][0] = estudiante.getIdEstudiante();
+            data[i][1] = estudiante.getNombre();
+            data[i][2] = estudiante.getApellidoPaterno();
+            data[i][3] = estudiante.getApellidoMaterno();
+            data[i][4] = estudiante.getFechaNacimiento();
+            data[i][5] = estudiante.getDni().getTipoDocumentoDni();
+            data[i][6] = estudiante.getDni().getNumeroDni();
+            data[i][7] = estudiante.getDireccion().getCalle();
+            data[i][8] = estudiante.getDireccion().getNumero();
+            data[i][9] = estudiante.getDireccion().getDistrito();
+            data[i][10] = estudiante.getDireccion().getProvincia();
+            data[i][11] = estudiante.getTelefono();
+            data[i][12] = estudiante.getEmailPersonal();
+            data[i][13] = estudiante.getCodigoEstudiante();
+            data[i][14] = estudiante.getEmailEducativo();
+        }
+        DefaultTableModel modeloTabla = new DefaultTableModel(data, titulosColumnas);
         frmEstudiantesView.btlData.setModel(modeloTabla);
+        System.out.println("Listando en tabla.....");
     }
 }
