d86608c038d6afadf4736eb3510279716d9303dd
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-24 23:31:30 -0500

    class DocenteReposioImple que implmenta los metodos de la interface Repositorio<Docente>

---
+++ src/com/Modelo/entidades/DaoImplementacion/DocenteReposioImple.java
@@ -0,0 +1,35 @@
+package com.Modelo.entidades.DaoImplementacion;
+
+import com.Modelo.entidades.Docente;
+import com.Modelo.entidades.InterfaceDao.Repositorio;
+import java.util.List;
+
+public class DocenteReposioImple
+        implements Repositorio<Docente> {
+    
+    @Override
+    public List<Docente> listar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    @Override
+    public Docente porDni(Long dni) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    @Override
+    public void guardar(Docente t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    @Override
+    public void modificar(Docente t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    @Override
+    public void eliminar(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
