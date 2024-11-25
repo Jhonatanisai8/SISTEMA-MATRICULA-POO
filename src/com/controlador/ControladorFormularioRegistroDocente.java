ecfdefbc13ee0ff6d2f5c7b0d73fa172fd165099
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-25 03:43:06 -0500

    CLASE ControladorFormularioRegistroDocente: programacion del evento para que se pueda guardar la informacion de un docente

--- src/com/controlador/ControladorFormularioRegistroDocente.java
+++ src/com/controlador/ControladorFormularioRegistroDocente.java
@@ -1,18 +1,36 @@
 package com.controlador;
 
+import com.Modelo.entidades.DaoImplementacion.DocenteReposioImple;
+import com.Modelo.entidades.Docente;
+import com.Modelo.entidades.InterfaceDao.Repositorio;
 import com.procesos.ProcesosFormularioRegistroDocente;
 import com.vista.frmMenuView;
 import com.vista.frmRegistrarDocenteView;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ControladorFormularioRegistroDocente {
-
+public class ControladorFormularioRegistroDocente
+        implements ActionListener {
+    
     private frmRegistrarDocenteView frmRegistrarDocenteView;
     private frmMenuView frmMenuView;
-
+    private Docente miDocente;
+    
     public ControladorFormularioRegistroDocente(frmRegistrarDocenteView frmRegistrarDocenteView, frmMenuView frmMenuView) {
         this.frmRegistrarDocenteView = frmRegistrarDocenteView;
         this.frmMenuView = frmMenuView;
+        this.frmRegistrarDocenteView.btnGuardar.addActionListener(this);
         ProcesosFormularioRegistroDocente.presentarFormulario(this.frmMenuView.dsktEscritorio, this.frmRegistrarDocenteView);
     }
-
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Repositorio<Docente> repo = new DocenteReposioImple();
+        //si el usuario da clik sobre guardar 
+        if (e.getSource() == this.frmRegistrarDocenteView.btnGuardar) {
+            miDocente = ProcesosFormularioRegistroDocente.crearDocenteFormulario(this.frmRegistrarDocenteView);
+            repo.guardar(miDocente);
+        }
+    }
+    
 }
