b00386123e251366872c042fd2aad3c8e17a7a5d
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-25 00:34:11 -0500

    clase ProcesosFormularioRegistroDocente: metodo para presentar el formulario

--- src/com/procesos/ProcesosFormularioRegistroDocente.java
+++ src/com/procesos/ProcesosFormularioRegistroDocente.java
@@ -1,11 +1,13 @@
 package com.procesos;
 
+import com.Modelo.entidades.Docente;
 import com.Utelerias.Constantes.ConstantesFormularioRegistroEstudianteDocente;
 import com.vista.frmRegistrarDocenteView;
+import javax.swing.JDesktopPane;
 
 public class ProcesosFormularioRegistroDocente
         implements ConstantesFormularioRegistroEstudianteDocente {
-    
+
     public static void rellenarComboBox(frmRegistrarDocenteView frmRegistrarDocenteView1) {
         frmRegistrarDocenteView1.cbxProvincia.removeAllItems();
         frmRegistrarDocenteView1.cbxTipoDocumento.removeAllItems();
@@ -20,10 +22,25 @@ public class ProcesosFormularioRegistroDocente
         for (String documento : TIPO_DOCUMENTO) {
             frmRegistrarDocenteView1.cbxTipoDocumento.addItem(documento);
         }
-        
+
     }
-    
+
     public static void ponerIconosFormulario(frmRegistrarDocenteView frmRegistrarDocenteView1) {
         frmRegistrarDocenteView1.btnGuardar.setIcon(ICONO_BOTON_GUARDAR);
     }
+
+    public static Docente crearDocente(frmRegistrarDocenteView frmRegistrarDocenteView1) {
+        return null;
+    }
+
+    public static void presentarFormulario(JDesktopPane desktopPane, frmRegistrarDocenteView frmRegistrarDocenteView1) {
+        rellenarComboBox(frmRegistrarDocenteView1);
+        ponerIconosFormulario(frmRegistrarDocenteView1);
+        frmRegistrarDocenteView1.setTitle("Datos del Docente: ");
+        desktopPane.removeAll();
+        desktopPane.add(frmRegistrarDocenteView1);
+        frmRegistrarDocenteView1.toFront();
+        frmRegistrarDocenteView1.setVisible(true);
+    }
+
 }
