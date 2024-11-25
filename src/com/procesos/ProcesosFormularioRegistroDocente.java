4d3f5408085171fbd3c21f8565b9f946ea379147
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-25 00:04:19 -0500

    clase ProcesosFormularioRegistroDocente: metodo para rellenar los combo box

---
+++ src/com/procesos/ProcesosFormularioRegistroDocente.java
@@ -0,0 +1,25 @@
+package com.procesos;
+
+import com.Utelerias.Constantes.ConstantesFormularioRegistroEstudianteDocente;
+import com.vista.frmRegistrarDocenteView;
+
+public class ProcesosFormularioRegistroDocente
+        implements ConstantesFormularioRegistroEstudianteDocente {
+
+    public static void rellenarComboBox(frmRegistrarDocenteView frmRegistrarDocenteView1) {
+        frmRegistrarDocenteView1.cbxProvincia.removeAllItems();
+        frmRegistrarDocenteView1.cbxTipoDocumento.removeAllItems();
+        frmRegistrarDocenteView1.cbxTituloAcademico.removeAllItems();
+        //rellamos los combo box
+        for (String provincia : PROVINCIAS) {
+            frmRegistrarDocenteView1.cbxProvincia.addItem(provincia);
+        }
+        for (String titulo : TITULOS_ACADEMICOS) {
+            frmRegistrarDocenteView1.cbxTituloAcademico.addItem(titulo);
+        }
+        for (String documento : TIPO_DOCUMENTO) {
+            frmRegistrarDocenteView1.cbxTipoDocumento.addItem(documento);
+        }
+
+    }
+}
