8c12ba5f194ec449d441958589bc3a08a199c254
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-25 02:50:46 -0500

    CLASE DocenteReposioImple: implementacion del formulario cliente

--- src/com/Modelo/entidades/DaoImplementacion/DocenteReposioImple.java
+++ src/com/Modelo/entidades/DaoImplementacion/DocenteReposioImple.java
@@ -1,12 +1,15 @@
 package com.Modelo.entidades.DaoImplementacion;
 
 import com.Modelo.entidades.BaseDatos.ConexionBaseDatos;
+import com.Modelo.entidades.Direccion;
+import com.Modelo.entidades.Dni;
 import com.Modelo.entidades.Docente;
 import com.Modelo.entidades.InterfaceDao.ConsultasSQLDocente;
 import com.Modelo.entidades.InterfaceDao.ConsultasSQLEstudiante;
 import com.Modelo.entidades.InterfaceDao.Repositorio;
 import java.util.List;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class DocenteReposioImple
         implements Repositorio<Docente>, ConsultasSQLEstudiante, ConsultasSQLDocente {
@@ -17,7 +20,47 @@ public class DocenteReposioImple
 
     @Override
     public List<Docente> listar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        List<Docente> listaDocentes = new ArrayList<>();
+        try (Connection con = getConection(); Statement stmt = con.createStatement(); ResultSet rs = stmt.executeQuery(SQL_SELECT_DOCENTES)) {
+            while (rs.next()) {
+                Docente docente = crearDocente(rs);
+                listaDocentes.add(docente);
+            }
+        } catch (Exception e) {
+            System.out.println("Error al listar los docentes: " + e.getMessage());
+        }
+        return listaDocentes;
+    }
+
+    private Docente crearDocente(final ResultSet rs) throws SQLException {
+        Docente docente = new Docente();
+        Dni dni = new Dni();
+        Direccion direccion = new Direccion();
+        docente.setIdDocente(rs.getLong("ID"));
+        docente.setIdPersona(rs.getLong("ID"));
+        docente.setNombre(rs.getString("Nombre"));
+        docente.setApellidoPaterno(rs.getString("Apellido Paterno"));
+        docente.setFechaNacimiento(rs.getDate("Fecha Nacimiento"));
+        docente.setTelefono(rs.getString("Telefono"));
+        docente.setEmailPersonal(rs.getString("Email Personal"));
+        docente.setCodigoDocente(rs.getString("Cod Docente"));
+        docente.setTituloAcademico(rs.getString("Titulo Academico"));
+        docente.setAniosExperiencia(rs.getInt("Anios Exp"));
+        docente.setEmailEducativoDocente(rs.getString("Email Educativo"));
+        //dni
+        dni.setIdDni(rs.getLong("ID"));
+        dni.setTipoDocumentoDni(rs.getString("Tipo Documento"));
+        dni.setNumeroDni(rs.getString("Numero Dni"));
+        //direccion
+        direccion.setIdDireccion(rs.getLong("ID"));
+        direccion.setCalle(rs.getString("Calle"));
+        direccion.setNumero(rs.getString("Numero Calle"));
+        direccion.setDistrito(rs.getString("Distrito"));
+        direccion.setProvincia(rs.getString("Provincia"));
+        //establemos al docente
+        docente.setDni(dni);
+        docente.setDireccion(direccion);
+        return docente;
     }
 
     @Override
