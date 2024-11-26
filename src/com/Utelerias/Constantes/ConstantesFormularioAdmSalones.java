4f6adaa0b8f5c140128165203f9ea18a8423046e
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-26 02:19:56 -0500

    constantes para el formulario adm-Salones

---
+++ src/com/Utelerias/Constantes/ConstantesFormularioAdmSalones.java
@@ -0,0 +1,6 @@
+package com.Utelerias.Constantes;
+
+public interface ConstantesFormularioAdmSalones {
+
+    final String[] TITULOS_COLUMNAS = {"Nº", "Nombre", "Cap. Estudiantes", "Referencia"};
+}
