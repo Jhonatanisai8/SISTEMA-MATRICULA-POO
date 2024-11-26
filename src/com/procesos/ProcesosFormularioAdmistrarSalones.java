b6efc3a2b86409ac0d29e98f5088d4af23e603c5
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-26 02:19:26 -0500

    CLASE ProcesosFormularioAdmistrarSalones: haciendo visible el formulario

--- src/com/procesos/ProcesosFormularioAdmistrarSalones.java
+++ src/com/procesos/ProcesosFormularioAdmistrarSalones.java
@@ -1,15 +1,28 @@
 package com.procesos;
 
+import com.Modelo.entidades.DaoImplementacion.SalonReposiImple;
+import com.Modelo.entidades.InterfaceDao.Repositorio;
+import com.Modelo.entidades.Salon;
+import com.Utelerias.Constantes.ConstantesFormularioAdmSalones;
+import com.procesos.Servicios.ServiciosAdmSalones;
 import com.vista.frmAdmistrarSalonesView;
+import java.util.List;
 import javax.swing.JDesktopPane;
 
-public class ProcesosFormularioAdmistrarSalones {
-
+public class ProcesosFormularioAdmistrarSalones
+        implements ConstantesFormularioAdmSalones {
+    
+    private static List<Salon> obtenerListaDocente() {
+        Repositorio<Salon> repo = new SalonReposiImple();
+        return repo.listar();
+    }
+    
     public static void presentarFormulario(JDesktopPane desktopPane, frmAdmistrarSalonesView frmSalonesView) {
         frmSalonesView.setTitle("Administracion de Salones.");
         desktopPane.removeAll();
         desktopPane.add(frmSalonesView);
         frmSalonesView.toFront();
         frmSalonesView.setVisible(true);
+        ServiciosAdmSalones.mostrarSalonesEnTabla(frmSalonesView, TITULOS_COLUMNAS, obtenerListaDocente());
     }
 }
