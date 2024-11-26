e1cae1efbe47410c60a9ba63f1273f4e863186b5
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-26 01:27:35 -0500

    consultas SQL para la tabla salon

---
+++ src/com/Modelo/entidades/InterfaceDao/ConsultasSQLSalon.java
@@ -0,0 +1,13 @@
+package com.Modelo.entidades.InterfaceDao;
+
+public interface ConsultasSQLSalon {
+
+    final String SQL_INSERT_SALON = "INSERT INTO salon (nombre_salon,capacidad,referencia) VALUES (?,?,?)";
+
+    final String SQL_UPDATE_SALON = "UPDATE salon SET nombre_salon = ? ,capacidad = ?,referencia = ? WHERE id_salon = ?";
+
+    final String SQL_DELETE_SALON = "DELETE FROM salon WHERE id_salon = ?";
+
+    final String SQL_BUSCAR_SALON = "SELECT id_salon,nombre_salon,capacidad,referencia FROM salon WHERE id_salon = ?";
+
+}
