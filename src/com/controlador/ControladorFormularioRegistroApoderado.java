6ab118f6085523357c2cb0e2086344fe932e819c
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-29 10:56:06 -0500

    clase ControladorFormularioRegistroApoderado: implementacion del evento del boton guardar, pero en este caso hara la funcionalidad de modificar o guardar segun sea el caso

--- src/com/controlador/ControladorFormularioRegistroApoderado.java
+++ src/com/controlador/ControladorFormularioRegistroApoderado.java
@@ -17,6 +17,7 @@ public class ControladorFormularioRegistroApoderado
     private final frmRegistrarApoderadoView frmRegistrarApoderadoView;
     private final frmMenuView frmMenuView;
     private Apoderado miApoderado;
+    private Apoderado apoderadoModificar;
 
     public ControladorFormularioRegistroApoderado(frmRegistrarApoderadoView frmRegistrarApoderadoView, frmMenuView frmMenuView) {
         this.frmRegistrarApoderadoView = frmRegistrarApoderadoView;
@@ -25,10 +26,19 @@ public class ControladorFormularioRegistroApoderado
         ProcesosFormularioRegistroApoderado.presentarFormulario(this.frmMenuView.dsktEscritorio, this.frmRegistrarApoderadoView);
     }
 
+    //constructor para la funcionalidad de modificar 
+    public ControladorFormularioRegistroApoderado(frmRegistrarApoderadoView frmRegistrarApoderadoView, frmMenuView frmMenuView, Apoderado apoderadoModificar) {
+        this.frmRegistrarApoderadoView = frmRegistrarApoderadoView;
+        this.frmMenuView = frmMenuView;
+        this.apoderadoModificar = apoderadoModificar;
+        this.frmRegistrarApoderadoView.btnGuardar.addActionListener(this);
+        this.frmRegistrarApoderadoView.btnGuardar.setText("Modificar");
+        ProcesosFormularioRegistroApoderado.presentarFormulario(this.frmMenuView.dsktEscritorio, this.frmRegistrarApoderadoView, this.apoderadoModificar);
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         Repositorio<Apoderado> repositorio = new ApoderadoReposiImple();
-
         if (e.getSource() == this.frmRegistrarApoderadoView.btnGuardar) {
             if (frmRegistrarApoderadoView.btnGuardar.getText().equalsIgnoreCase("guardar")) {
                 miApoderado = ProcesosFormularioRegistroApoderado.crearApoderadoDesdeFormulario(frmRegistrarApoderadoView);
@@ -36,12 +46,19 @@ public class ControladorFormularioRegistroApoderado
                     repositorio.guardar(miApoderado);
                     ServiciosApoderado.limpiarDatos(frmRegistrarApoderadoView);
                     JOptionPane.showMessageDialog(null, "Apoderado con nombre: " + miApoderado.getNombre(), "Atención", 3);
-                    System.out.println("Click para guardar un apoderado....");
                 } else {
                     JOptionPane.showMessageDialog(null, "Error, no se guardo el Apoderado.", "Atención", 3);
                 }
+                System.out.println("Click para guardar un apoderado....");
+            } else {
+                Long idApoderado = this.apoderadoModificar.getIdApoderado();
+                Apoderado apoderadoModificado = ProcesosFormularioRegistroApoderado.crearApoderadoDesdeFormulario(this.frmRegistrarApoderadoView);
+                apoderadoModificado.setIdApoderado(idApoderado);
+                repositorio.modificar(apoderadoModificado);
+                JOptionPane.showMessageDialog(null, "Apoderado con ID: " + apoderadoModificado.getIdApoderado() + " \nModificado Correctamente!.", "Atencion", 3);
+                ServiciosApoderado.limpiarDatos(frmRegistrarApoderadoView);
             }
-            System.out.println("Clicl en el boton para guardar....");
+            System.out.println("Clicl en el boton para modificar....");
         }
     }
 
