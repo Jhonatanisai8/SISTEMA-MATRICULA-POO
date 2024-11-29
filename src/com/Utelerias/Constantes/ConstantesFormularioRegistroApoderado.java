41c50ba0f82aae19034130828ebf969b13cf429f
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-28 22:41:00 -0500

    constantes necesarias para el formulario de registro de un apoderado

---
+++ src/com/Utelerias/Constantes/ConstantesFormularioRegistroApoderado.java
@@ -0,0 +1,24 @@
+package com.Utelerias.Constantes;
+
+import javax.swing.ImageIcon;
+
+public interface ConstantesFormularioRegistroApoderado {
+
+    final ImageIcon ICONO_BOTON_GUARDAR = new ImageIcon("C:\\Users\\User\\Music\\CUARTO CICLO\\08 LABORATORIOS POO\\ProyectoMatriculaPOO\\src\\com\\imgs\\RegistroEstudiantes.png");
+    final String[] PROVINCIAS = {"=Seleccionar=", "Amazonas", "Ancash", "Arequipa", "Ayacucho", "Cajamarca", "Callao", "Cuzco", "Lima"};
+    final String[] TIPO_DOCUMENTO = {"=Seleccionar=", "Dni", "Carnet De Extranjeria"};
+    final String[] RELACION_ESTUDIANTE = {"=Seleccionar=", "Madre", "Padre", "Tutor Legal", "Abuelo", "Abuela"};
+    final String[] OCUPACIONES = {
+        "=Seleccionar=", "Programador", "Desarrollador web", "Analista de datos", "Ingeniero de software",
+        "Diseñador gráfico", "Diseñador UX/UI", "Arquitecto de software", "Tester de software",
+        "Cientifico de datos", "Ingeniero de datos", "Especialista en SEO", "Especialista en marketing digital",
+        "Community manager", "Content manager", "Gerente de proyecto", "Consultor",
+        "Doctor", "Enfermero", "Veterinario", "Farmacéutico",
+        "Profesor", "Investigador", "Abogado", "Juez",
+        "Artista", "Músico", "Escritor", "Actor",
+        "Chef", "Mecánico", "Electricista"
+    };
+    final String[] ESTADO_CIVIL = {"=Seleccionar=", "Soltero", "Casado"};
+    final String[] TITULOS_ACADEMICOS = {"=Seleccionar=", "Licenciatura", "Maestría", "Doctorado", "Especializaciones y Diplomados"};
+
+}
