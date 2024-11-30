15facffe5c435dad826e387044fcd30c24a37451
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-30 00:41:30 -0500

    modificacion de la consulta para insertar un estudiante

--- src/com/Modelo/entidades/InterfaceDao/ConsultasSQLEstudiante.java
+++ src/com/Modelo/entidades/InterfaceDao/ConsultasSQLEstudiante.java
@@ -17,8 +17,8 @@ public interface ConsultasSQLEstudiante {
     final String SQL_INSERT_PERSONA = "INSERT INTO persona (nombre,apellido_paterno,apellido_materno,fecha_nacimiento,id_dni,id_direccion,telefono,email_personal) "
             + " VALUES (?,?,?,?,(SELECT id_dni FROM dni ORDER BY id_dni DESC limit 1),(SELECT id_direccion FROM direccion ORDER BY id_direccion DESC limit 1),?,?)";
 
-    final String SQL_INSERT_ESTUDIANTE = "INSERT INTO estudiante (id_estudiante,codigo_estudiante,email_educativo)"
-            + " VALUES ((SELECT id_persona FROM persona ORDER BY id_persona DESC limit 1),?,?)";
+    final String SQL_INSERT_ESTUDIANTE = "INSERT INTO estudiante (id_estudiante,codigo_estudiante,email_educativo,id_apoderado)"
+            + " VALUES ((SELECT id_persona FROM persona ORDER BY id_persona DESC limit 1),?,?,?)";
 
     final String SQL_UPDATE_DNI = "UPDATE  dni SET tipo_documento = ?, numero_dni = ? WHERE id_dni = ?";
 
