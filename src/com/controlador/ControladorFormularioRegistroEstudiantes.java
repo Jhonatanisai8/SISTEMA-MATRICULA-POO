22a40f6a4415d53685ddbf24f7dfd65bd497ba5a
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-30 01:18:21 -0500

    correcion del parametro de una consultas y ademas funcionalidad en el evento de guardar en el formulario registro

--- src/com/controlador/ControladorFormularioRegistroEstudiantes.java
+++ src/com/controlador/ControladorFormularioRegistroEstudiantes.java
@@ -40,20 +40,25 @@ public class ControladorFormularioRegistroEstudiantes
     public void actionPerformed(ActionEvent e) {
         Repositorio<Estudiante> repositorio = new EstudianteReposioImple();  // Asegúrate que el nombre sea correcto.
         if (e.getSource() == this.frEstudiante.btnGuardar) {
+            int fila = this.frEstudiante.tblApoderados.getSelectedRow();
             //si esta en guardar
             if (!frEstudiante.btnGuardar.getText().equalsIgnoreCase("Modificar")) {
-                estudiante = ProcesosFormularioRegistroEstudiante.crearEstudiante(this.frEstudiante);
-                if (estudiante != null) {
-                    repositorio.guardar(estudiante);
-                    ServiciosEstudianteDocente.limpiarDatos(frEstudiante);
-                    System.out.println("Has dado click para guardar.....");
-                    JOptionPane.showMessageDialog(null, "Estudiante con Nombre: " + estudiante.getNombre() + "\n guardado correctamente", "Atención", 3);
+                if (fila >= 0) {
+                    estudiante = ProcesosFormularioRegistroEstudiante.crearEstudiante(this.frEstudiante, fila);
+                    if (estudiante != null) {
+                        repositorio.guardar(estudiante);
+                        ServiciosEstudianteDocente.limpiarDatos(frEstudiante);
+                        System.out.println("Has dado click para guardar.....");
+                        JOptionPane.showMessageDialog(null, "Estudiante con Nombre: " + estudiante.getNombre() + "\n guardado correctamente", "Atención", 3);
+                    } else {
+                        JOptionPane.showMessageDialog(null, "No se puedo insertar el estudiante", "Atención", 3);
+                    }
                 } else {
-                    JOptionPane.showMessageDialog(null, "No se puedo insertar el estudiante", "Atención", 3);
+                    JOptionPane.showMessageDialog(null, "Por favor seleccinar una fila del apoderado.", "Atención", 3);
                 }
             } else {//si esta en modificar
                 Long id = this.estudianteMoficar.getIdEstudiante();
-                Estudiante estudianteModificar = ProcesosFormularioRegistroEstudiante.crearEstudiante(this.frEstudiante);
+                Estudiante estudianteModificar = ProcesosFormularioRegistroEstudiante.crearEstudiante(this.frEstudiante, fila);
                 estudianteModificar.setIdEstudiante(id);  // O si usas otro ID, asignarlo aquí.
                 repositorio.modificar(estudianteModificar);
                 JOptionPane.showMessageDialog(null, "Estudiante con Id: " + estudianteModificar.getIdEstudiante() + "\n modifcado correctamente", "Atención", 3);
