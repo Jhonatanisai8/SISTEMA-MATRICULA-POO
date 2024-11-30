03f91cc0d0e6c1f5da8ceba127d4466de55afd81
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-30 11:07:04 -0500

    clase ConstantesFormularioAdmCursos: serviran para el formulario de registro de un curso

---
+++ src/com/Utelerias/Constantes/ConstantesFormularioAdmCursos.java
@@ -0,0 +1,11 @@
+package com.Utelerias.Constantes;
+
+import java.awt.Font;
+
+public interface ConstantesFormularioAdmCursos {
+
+    final String ENCABEZADO_COLUMNAS_TABLA[] = {"ID", "CODIGO", "NOMBRE", "DESCRIPCION", "GRADO", "NIVEL"};
+    final Font FUENTE_TEXT_PRESENTACION = new Font("Segoe UI", Font.PLAIN, 12);
+    final String[] LISTA_GRADOS = {"=SELECCIONAR=", "1", "2", "3", "4", "5", "6"};
+
+}
