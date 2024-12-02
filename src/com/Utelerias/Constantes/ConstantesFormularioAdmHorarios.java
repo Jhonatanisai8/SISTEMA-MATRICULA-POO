875c0dbd9b3565988c94d869317d0ac76e481da1
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-02 10:45:18 -0500

    CLASE ConstantesFormularioAdmHorarios: coreccion de las horas

--- src/com/Utelerias/Constantes/ConstantesFormularioAdmHorarios.java
+++ src/com/Utelerias/Constantes/ConstantesFormularioAdmHorarios.java
@@ -3,8 +3,8 @@ package com.Utelerias.Constantes;
 public interface ConstantesFormularioAdmHorarios {
 
     final String[] DIA_SEMANA = {"=SELECIONAR=", "LUNES", "MARTES", "MIERCOLES", "JUEVES", "VIERNES"};
-    final String[] HORA_INICIO = {"=SELECIONAR=", "8:00 AM", "10:00 AM", "12:00 PM"};
-    final String[] HORA_FIN = {"=SELECIONAR=", "12:00 PM", "2:00 PM", "4:00 PM"};
+    final String[] HORA_INICIO = {"=SELECIONAR=", "08:00", "10:00", "12:00"};
+    final String[] HORA_FIN = {"=SELECIONAR=", "12:00", "02:00", "04:00"};
     final String[] TURNO = {"=SELECIONAR=", "MAÑANA", "TARDE"};
 
 }
