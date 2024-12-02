92b043c6328e9b7bdec5ed084509477c7556b788
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-02 08:00:59 -0500

    NUeva entidad Horario con sus atributos y metodos

---
+++ src/com/Modelo/entidades/Horario.java
@@ -0,0 +1,56 @@
+package com.Modelo.entidades;
+
+import java.time.LocalTime;
+
+public class Horario {
+
+    private Long idHorario;
+    private String diaSemana;
+    private LocalTime horaInicio;
+    private LocalTime horaFin;
+    private String turno;
+
+    public Horario() {
+    }
+
+    public Long getIdHorario() {
+        return idHorario;
+    }
+
+    public void setIdHorario(Long idHorario) {
+        this.idHorario = idHorario;
+    }
+
+    public String getDiaSemana() {
+        return diaSemana;
+    }
+
+    public void setDiaSemana(String diaSemana) {
+        this.diaSemana = diaSemana;
+    }
+
+    public LocalTime getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(LocalTime horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public LocalTime getHoraFin() {
+        return horaFin;
+    }
+
+    public void setHoraFin(LocalTime horaFin) {
+        this.horaFin = horaFin;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+
+}
