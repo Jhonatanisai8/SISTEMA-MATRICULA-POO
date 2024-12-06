1470ca0373af778828da0904f6db6bb48fef6133
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-06 13:43:10 -0500

    AGREGANGO VALIDACIONES PARA LA SELECCION DE FILAS EN LAS RESPECTIVAS TABLAS

---
+++ src/com/procesos/Validaciones/ValidacionesFormularioRegistrarAsignacion.java
@@ -0,0 +1,30 @@
+package com.procesos.Validaciones;
+
+import javax.swing.JTable;
+
+public class ValidacionesFormularioRegistrarAsignacion {
+
+    public static String validarSeleccionFilaTablas(
+            JTable tblCursos, JTable tblDocentes, JTable tblHorarios, JTable tblSalones
+    ) {
+        if (tblDocentes.getSelectedRow() < 0) {
+            return " de Tabla de Cursos.";
+        }
+        if (tblSalones.getSelectedRow() < 0) {
+            return " de Tabla de Docentes.";
+        }
+        if (tblCursos.getSelectedRow() < 0) {
+            return " de Tabla de Horarios.";
+        }
+        if (tblHorarios.getSelectedRow() < 0) {
+            return " de Tabla de Salones.";
+        }
+        if (tblCursos.getSelectedRow() < 0
+                && tblDocentes.getSelectedRow() < 0
+                && tblHorarios.getSelectedRow() < 0
+                && tblSalones.getSelectedRow() < 0) {
+            return "Por favor selccionar Fila de Docente, Horarios, Salones, Cursos.";
+        }
+        return "";
+    }
+}
