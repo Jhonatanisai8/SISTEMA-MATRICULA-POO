b821c9096112a6152dd4721a645873671acad6aa
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-06 23:28:47 -0500

    CLASE ValidacionesFormularioRegistrarAsignacion: reordenando el orden de la validacion de las tablas

--- src/com/procesos/Validaciones/ValidacionesFormularioRegistrarAsignacion.java
+++ src/com/procesos/Validaciones/ValidacionesFormularioRegistrarAsignacion.java
@@ -8,16 +8,16 @@ public class ValidacionesFormularioRegistrarAsignacion {
             JTable tblCursos, JTable tblDocentes, JTable tblHorarios, JTable tblSalones
     ) {
         if (tblDocentes.getSelectedRow() < 0) {
-            return " de Tabla de Cursos.";
-        }
-        if (tblSalones.getSelectedRow() < 0) {
             return " de Tabla de Docentes.";
         }
         if (tblCursos.getSelectedRow() < 0) {
-            return " de Tabla de Horarios.";
+            return " de Tabla de Cursos.";
+        }
+        if (tblSalones.getSelectedRow() < 0) {
+            return " de Tabla de Salones.";
         }
         if (tblHorarios.getSelectedRow() < 0) {
-            return " de Tabla de Salones.";
+            return " de Tabla de Horarios.";
         }
         if (tblCursos.getSelectedRow() < 0
                 && tblDocentes.getSelectedRow() < 0
