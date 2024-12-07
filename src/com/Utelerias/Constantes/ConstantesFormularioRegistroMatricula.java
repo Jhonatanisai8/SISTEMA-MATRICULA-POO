373e0701f33c09833def1a2cd6c7c66dd9ba1b69
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-07 10:28:30 -0500

    CLASE ProcesosFormularioRegistroMatricula: llamando a los metodos de la clase de servicios

---
+++ src/com/Utelerias/Constantes/ConstantesFormularioRegistroMatricula.java
@@ -0,0 +1,10 @@
+package com.Utelerias.Constantes;
+
+public interface ConstantesFormularioRegistroMatricula {
+
+    final String COLUMNAS_TABLA_ESTUDIANTES[] = {
+        "ID",
+        "NOMBRES Y APELLIDOS",
+        "COD.",};
+    final int TAMANIO_COLUMNAS[] = {30, 170, 60};
+}
--- src/com/procesos/ProcesosFormularioRegistroMatricula.java
+++ src/com/procesos/ProcesosFormularioRegistroMatricula.java
@@ -1,20 +1,31 @@
 package com.procesos;
 
+import com.Modelo.entidades.DaoImplementacion.EstudianteReposioImple;
+import static com.Utelerias.Constantes.ConstantesFormularioListadoEstudiantesDocentes.FUENTE_TEXT_PRESENTACION;
+import com.Utelerias.Constantes.ConstantesFormularioRegistroMatricula;
+import com.procesos.Servicios.ServiciosMatricula;
 import com.vista.frmRegistrarMatriculasView;
 import javax.swing.JDesktopPane;
 
-public class ProcesosFormularioRegistroMatricula {
-    
+public class ProcesosFormularioRegistroMatricula
+        implements ConstantesFormularioRegistroMatricula {
+
     static void ponerIconosFormulario(frmRegistrarMatriculasView frmRegistrarMatriculasView) {
-        
+
     }
-    
+
     public static void presentarFormulario(JDesktopPane escritorio, frmRegistrarMatriculasView frmRegistrarMatriculasView) {
+        EstudianteReposioImple estudianteReposioImple = new EstudianteReposioImple();
         ponerIconosFormulario(frmRegistrarMatriculasView);
         escritorio.removeAll();
         escritorio.add(frmRegistrarMatriculasView);
         frmRegistrarMatriculasView.toFront();
         frmRegistrarMatriculasView.setTitle("REGISTRO DE MATRICULA.");
         frmRegistrarMatriculasView.setVisible(true);
+        ServiciosMatricula.mostrarEstudiantesEnTabla(frmRegistrarMatriculasView.tblEstudiantes, COLUMNAS_TABLA_ESTUDIANTES, estudianteReposioImple.listar());
+        ServiciosMatricula.establecerAnchoColumnasTabla(frmRegistrarMatriculasView.tblEstudiantes, TAMANIO_COLUMNAS);
+        frmRegistrarMatriculasView.tblAsignaciones.setFont(FUENTE_TEXT_PRESENTACION);
+        frmRegistrarMatriculasView.tblEstudiantes.setFont(FUENTE_TEXT_PRESENTACION);
     }
+
 }
