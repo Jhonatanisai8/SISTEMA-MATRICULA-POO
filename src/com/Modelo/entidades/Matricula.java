b136fa682882c82c725e9293738fb56704ce2f14
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-06 23:39:58 -0500

    nueva entidad matricula con sus respectivos atributos y metodos

---
+++ src/com/Modelo/entidades/Matricula.java
@@ -0,0 +1,47 @@
+package com.Modelo.entidades;
+
+import java.util.Date;
+
+public class Matricula {
+
+    private Long idMatricula;
+    private Estudiante estudiante;
+    private Asignacion asignacion;
+    private Date fechaEmision;
+
+    public Matricula() {
+    }
+
+    public Long getIdMatricula() {
+        return idMatricula;
+    }
+
+    public void setIdMatricula(Long idMatricula) {
+        this.idMatricula = idMatricula;
+    }
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public Asignacion getAsignacion() {
+        return asignacion;
+    }
+
+    public void setAsignacion(Asignacion asignacion) {
+        this.asignacion = asignacion;
+    }
+
+    public Date getFechaEmision() {
+        return fechaEmision;
+    }
+
+    public void setFechaEmision(Date fechaEmision) {
+        this.fechaEmision = fechaEmision;
+    }
+
+}
