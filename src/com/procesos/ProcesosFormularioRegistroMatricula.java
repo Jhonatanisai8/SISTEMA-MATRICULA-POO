53971f4d019e6c05d381a008faaedf504a3729e9
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-07 13:58:28 -0500

    CLASE ProcesosFormularioRegistroMatricula: implementacion del metodo para crear un objeto de la clase matricula desde el formulario

--- src/com/procesos/ProcesosFormularioRegistroMatricula.java
+++ src/com/procesos/ProcesosFormularioRegistroMatricula.java
@@ -1,11 +1,15 @@
 package com.procesos;
 
+import com.Modelo.entidades.Asignacion;
 import com.Modelo.entidades.DaoImplementacion.EstudianteReposioImple;
 import com.Modelo.entidades.DaoImplementacion.MatriculaRepoImple;
+import com.Modelo.entidades.Estudiante;
+import com.Modelo.entidades.Matricula;
 import static com.Utelerias.Constantes.ConstantesFormularioListadoEstudiantesDocentes.FUENTE_TEXT_PRESENTACION;
 import com.Utelerias.Constantes.ConstantesFormularioRegistroMatricula;
 import com.procesos.Servicios.ServiciosMatricula;
 import com.vista.frmRegistrarMatriculasView;
+import java.util.Date;
 import javax.swing.JDesktopPane;
 
 public class ProcesosFormularioRegistroMatricula
@@ -32,4 +36,22 @@ public class ProcesosFormularioRegistroMatricula
         ServiciosMatricula.establecerAnchoColumnasTabla(frmRegistrarMatriculasView.tblAsignaciones, ANCHO_COLUMAS);
     }
 
+    public static Matricula crearMatriculaDesdeFormulario(frmRegistrarMatriculasView frmRegistrarMatriculasVie) {
+        Matricula matricula = new Matricula();
+        Asignacion asignacion = new Asignacion();
+        Estudiante estudiante = new Estudiante();
+        //obetenemos los ids
+        int filaSelecEstudiante = frmRegistrarMatriculasVie.tblEstudiantes.getSelectedRow();
+        int filaSelecAsignacion = frmRegistrarMatriculasVie.tblAsignaciones.getSelectedRow();
+        Long idEstudiante = (Long) frmRegistrarMatriculasVie.tblEstudiantes.getValueAt(filaSelecEstudiante, 0);
+        Long idAsgnacion = (Long) frmRegistrarMatriculasVie.tblEstudiantes.getValueAt(filaSelecAsignacion, 0);
+        //le establcemos los ids a los objetos que conforman la matricula
+        asignacion.setIdAsignacion(idAsgnacion);
+        estudiante.setIdEstudiante(idEstudiante);
+        //establcemos los objetos que conforman la matricula
+        matricula.setEstudiante(estudiante);
+        matricula.setAsignacion(asignacion);
+        matricula.setFechaEmision(new Date());
+        return matricula;
+    }
 }
