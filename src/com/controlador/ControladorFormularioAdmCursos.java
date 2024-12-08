003fb93c48a3362840a8868a69dd629cc068c86d
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-08 14:03:14 -0500

    CLASE ControladorFormularioAdmCursos: metodo clicksobreTabla

--- src/com/controlador/ControladorFormularioAdmCursos.java
+++ src/com/controlador/ControladorFormularioAdmCursos.java
@@ -11,22 +11,40 @@ import com.vista.frmAdmistrarCursos;
 import com.vista.frmMenuView;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.JOptionPane;
 
 public class ControladorFormularioAdmCursos
         implements ActionListener {
-
+    
     private final frmMenuView frmMenuView;
     private final frmAdmistrarCursos frmAdmistrarCursos;
     private Curso miCurso;
-
+    private Long id;
+    
     public ControladorFormularioAdmCursos(frmMenuView frmMenuView, frmAdmistrarCursos frmAdmistrarCursos) {
         this.frmMenuView = frmMenuView;
         this.frmAdmistrarCursos = frmAdmistrarCursos;
         this.frmAdmistrarCursos.btnGuardarCurso.addActionListener(this);
+        this.frmAdmistrarCursos.btnModificar.addActionListener(this);
         ProcesosFormularioAdmistrarCursos.presentarFormulario(this.frmMenuView.dsktEscritorio, this.frmAdmistrarCursos);
+        clicksobreTabla();
     }
-
+    
+    private void clicksobreTabla() {
+        frmAdmistrarCursos.tblDatosCursos.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int filaSelecionada = frmAdmistrarCursos.tblDatosCursos.getSelectedRow();
+                //obetnemos el id del curso de la fila seleccionada
+                id = Long.valueOf(frmAdmistrarCursos.tblDatosCursos.getValueAt(filaSelecionada, 0).toString());
+                Curso cursoModificado = ProcesosFormularioAdmistrarCursos.obtenerCurso(id);
+                ProcesosFormularioAdmistrarCursos.presentarInformacionCurso(frmAdmistrarCursos, cursoModificado);
+            }
+        });
+    }
+    
     @Override
     public void actionPerformed(ActionEvent e) {
         Repositorio<Curso> repo = new CursoReposiImple();
@@ -42,6 +60,23 @@ public class ControladorFormularioAdmCursos
                 JOptionPane.showMessageDialog(null, "Curso Registrado Correctamente", "ATENCIÓN", 3);
             }
         }
+        
+        if (e.getSource() == this.frmAdmistrarCursos.btnModificar) {
+            int filaSelecionada = frmAdmistrarCursos.tblDatosCursos.getSelectedRow();
+            if (filaSelecionada < 0) {
+                JOptionPane.showMessageDialog(null, "Por favor selecione una fila.", "ATENCION", JOptionPane.INFORMATION_MESSAGE);
+                return;
+            }
+            this.miCurso = ProcesosFormularioAdmistrarCursos.creaCursoFormulario(this.frmAdmistrarCursos);
+            this.miCurso.setIdCurso(id);
+            repo.modificar(this.miCurso);
+            admCurso.mostrarRegistrosEnTabla(frmAdmistrarCursos.tblDatosCursos, ENCABEZADO_COLUMNAS_TABLA, repo.listar());
+            admCurso.establecerAnchoColumnasTabla(this.frmAdmistrarCursos.tblDatosCursos, ANCH0_COLUMNAS);
+            ServiciosCurso.limpiarCampos(this.frmAdmistrarCursos);
+            this.frmAdmistrarCursos.btnGuardarCurso.setEnabled(true);
+            JOptionPane.showMessageDialog(null, "Curso Modificado Correctamente", "ATENCIÓN", 3);
+            System.out.println("Click sobre el boton modificar....");
+        }
     }
-
+    
 }
