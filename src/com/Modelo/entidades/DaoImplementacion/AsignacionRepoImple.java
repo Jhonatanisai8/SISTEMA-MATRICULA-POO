efe0c9e3f1422da06abb5ed7ab15f7823978ccca
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-09 19:32:38 -0500

    CLASE AsignacionRepoImple: implementacion del metodo para listar asignaciones

--- src/com/Modelo/entidades/DaoImplementacion/AsignacionRepoImple.java
+++ src/com/Modelo/entidades/DaoImplementacion/AsignacionRepoImple.java
@@ -2,9 +2,14 @@ package com.Modelo.entidades.DaoImplementacion;
 
 import com.Modelo.entidades.Asignacion;
 import com.Modelo.entidades.BaseDatos.ConexionBaseDatos;
+import com.Modelo.entidades.Curso;
+import com.Modelo.entidades.Docente;
+import com.Modelo.entidades.Horario;
 import com.Modelo.entidades.InterfaceDao.ConsultasSQLAsignacion;
 import com.Modelo.entidades.InterfaceDao.Repositorio;
+import com.Modelo.entidades.Salon;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import javax.swing.table.DefaultTableModel;
 
@@ -18,7 +23,49 @@ public class AsignacionRepoImple
 
     @Override
     public List<Asignacion> listar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        List<Asignacion> listaAsignaccion = new ArrayList<>();
+        try (Connection con = getConnection(); Statement st = con.createStatement(); ResultSet rs = st.executeQuery(SQL_INSERT_ASIGNACION)) {
+            while (rs.next()) {
+                Asignacion asignacion = CrearAsignacion(rs);
+                listaAsignaccion.add(asignacion);
+            }
+        } catch (Exception e) {
+            System.out.println("Error al listar las asignaciones: " + e.getMessage());
+        }
+        return listaAsignaccion;
+    }
+
+    private Asignacion CrearAsignacion(final ResultSet rs) throws SQLException {
+        Asignacion asignacion = new Asignacion();
+        asignacion.setIdAsignacion(rs.getLong("ID Asignacion"));
+        Curso curso = new Curso();
+        curso.setIdCurso(rs.getLong("ID Curso"));
+        curso.setNombreCurso(rs.getString("Codigo Curso"));
+        curso.setDescripcion(rs.getString("Descripcion Curso"));
+        curso.setGrado(rs.getInt("Grado"));
+        curso.setNivel(rs.getString("Nivel"));
+        asignacion.setCurso(curso);
+        Docente docente = new Docente();
+        docente.setIdDocente(rs.getLong("ID Docente"));
+        docente.setNombre(rs.getString("Nombre Docente"));
+        docente.setApellidoPaterno(rs.getString("Apellido Paterno Docente"));
+        docente.setApellidoMaterno(rs.getString("Apellido Materno Docente"));
+        docente.setTelefono(rs.getString("Telefono Docente"));
+        asignacion.setDocente(docente);
+        Salon salon = new Salon();
+        salon.setIdSalon(rs.getLong("ID Salon"));
+        salon.setNombreSalon(rs.getString("Nombre Salon"));
+        salon.setCapacidadEstudiantes(rs.getInt("Capacidad Salon"));
+        salon.setReferencia(rs.getString("Referencia Salon"));
+        asignacion.setSalon(salon);
+        Horario horario = new Horario();
+        horario.setIdHorario(rs.getLong("ID Horario"));
+        horario.setDiaSemana(rs.getString("Día Semana"));
+        horario.setHoraInicio(rs.getTime("Hora Inicio").toLocalTime());
+        horario.setHoraFin(rs.getTime("Hora Fin").toLocalTime());
+        horario.setTurno(rs.getString("Turno"));
+        asignacion.setHorario(horario);
+        return asignacion;
     }
 
     @Override
@@ -53,7 +100,7 @@ public class AsignacionRepoImple
 
     public DefaultTableModel listarInformacionObjeto(DefaultTableModel model, String parametro, int bandera) {
         model.setRowCount(0);
-        
+
         return model;
     }
 
--- src/com/Modelo/entidades/InterfaceDao/ConsultasSQLAsignacion.java
+++ src/com/Modelo/entidades/InterfaceDao/ConsultasSQLAsignacion.java
@@ -3,4 +3,40 @@ package com.Modelo.entidades.InterfaceDao;
 public interface ConsultasSQLAsignacion {
 
     final String SQL_INSERT_ASIGNACION = "INSERT INTO asignacion(id_docente,id_curso,id_salon,id_horario) VALUES (?,?,?,?)";
+
+    final String SQL_SELECT_ASIGNACIONES = """
+                                           SELECT 
+                                               a.id_asignacion AS "ID Asignacion",
+                                               c.id_curso AS "ID Curso",
+                                               c.codigo_curso AS "Codigo Curso",
+                                               c.nombre_curso AS "Nombre Curso",
+                                               c.descripcion AS "Descripcion Curso",
+                                               c.grado AS "Grado",
+                                               c.nivel AS "Nivel",
+                                               d.id_docente AS "ID Docente",
+                                               p.nombre AS "Nombre Docente",
+                                               p.apellido_paterno AS "Apellido Paterno Docente",
+                                               p.apellido_materno AS "Apellido Materno Docente",
+                                               p.telefono AS "Telefono Docente",
+                                               s.id_salon AS "ID Salon",
+                                               s.nombre_salon AS "Nombre Salon",
+                                               s.capacidad AS "Capacidad Salon",
+                                               s.referencia AS "Referencia Salon",
+                                               h.id_horario AS "ID Horario",
+                                               h.dia_semana AS "D\u00eda Semana",
+                                               h.hora_inicio AS "Hora Inicio",
+                                               h.hora_fin AS "Hora Fin",
+                                               h.turno AS "Turno"
+                                           FROM 
+                                               asignacion a
+                                           JOIN 
+                                               curso c ON a.id_curso = c.id_curso
+                                           JOIN 
+                                               docente d ON a.id_docente = d.id_docente
+                                           JOIN 
+                                               persona p ON d.id_docente = p.id_persona
+                                           JOIN 
+                                               salon s ON a.id_salon = s.id_salon
+                                           JOIN 
+                                               horario h ON a.id_horario = h.id_horario""";
 }
